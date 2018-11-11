226c66b1c44e232b4cf881a2c9380be901795964
Kevin Yang <chgc_kevin@hotmail.com>
2018-11-11 11:21:33 +0800

    加入 RouterModule 與 路由設定檔

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Route } from '@angular/router';
 import { AppComponent } from './app.component';
+import { Page1Component } from './page1/page1.component';
+
+const routes: Route[] = [
+  { path: 'page1', component: Page1Component },
+  { path: '**', redirectTo: '/page1', pathMatch: 'full' }
+];
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule, RouterModule],
+  declarations: [AppComponent, Page1Component],
+  imports: [BrowserModule, HttpClientModule, RouterModule.forRoot(routes)],
   providers: [],
   bootstrap: [AppComponent]
 })
